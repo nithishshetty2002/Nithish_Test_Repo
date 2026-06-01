f3d5dc96fd6c4aa4f954e6fa174d7ad7aad3aa46
nithishshetty2002 <nithishshetty2002@gmail.com>
2026-06-01 23:25:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
-    public static void main(String[] args) {
-        logger.info("Application started");
-    }
+  public static void main[] {
+    System.out.println("Hello120");
+  }
 }
