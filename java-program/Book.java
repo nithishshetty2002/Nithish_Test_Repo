a177bd651cec265e92b15065a61fb080b4fda3e1
nithishshetty2002 <nithishshetty2002@gmail.com>
2026-06-03 14:24:47 +0530

    Create Book.java

---
+++ java-program/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private final String title;
+    private final String author;
+    private int copies;
+
+    public Book(String title, String author, int copies) {
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void borrowOneCopy() {
+        if (copies > 0) {
+            copies--;
+        }
+    }
+}
