44666f6adcd619474291ed31e8a461d9c65d857b
nithishshetty2002 <nithishshetty2002@gmail.com>
2026-06-03 14:27:09 +0530

    Create Library.java

---
+++ java-program/Library.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Library {
+    private final String name;
+    private final List<Book> books = new ArrayList<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return Collections.unmodifiableList(books);
+    }
+
+    public int getTotalCopies() {
+        int total = 0;
+        for (Book book : books) {
+            total += book.getCopies();
+        }
+        return total;
+    }
+}
